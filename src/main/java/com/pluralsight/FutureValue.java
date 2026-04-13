a1bbb602d8cdcc5f473d2b0445b905e6c4815c57
pmvroque15 <pmvroque@gmail.com>
2026-04-13 09:34:28 -0400

    Added more prompts.

--- src/main/java/com/pluralsight/FutureValue.java
+++ src/main/java/com/pluralsight/FutureValue.java
@@ -10,14 +10,22 @@ public class FutureValue {
         //Make a greeting prompt
         System.out.println("Hello! Welcome to the Future Value calculator");
         System.out.println("----------------------------------------------");
+        //prompt asks the user's name
+        System.out.println("Type your first and last name:");
+        String userName = scanner.nextLine();
         //Asking for Principal (P): This is the initial deposit amount
-
+        System.out.println("What is the initial deposit amount");
+        double userPrincipalAmount = scanner.nextDouble();
         //Asking for Annual Interest Rate (r): The nominal annual interest rate in decimal form.
+        System.out.println("Annual Interest Rate:");
+        double userAnnualInterestRate = scanner.nextDouble();
         //Asking for Number of Years (t): The total number of years the deposit will earn interest.
-        //Asking for Days Per Year: Daily compounding assumes 365 days per year
-        // It would accept the deposit, interest rate, and number of years from the user
-        // It would display the future value and the total interest earned
-        //Display Total Number of Days:
+        System.out.println("Enter how many years you plan to leave the money in the account: ");
+        double userNumberOfYears = scanner.nextDouble();
+        //Days Per Year: Daily compounding assumes 365 days per year
+            // It would accept the deposit, interest rate, and number of years from the user
+            // It would display the future value and the total interest earned
+            //Display Total Number of Days:
             // - This is 365 × t (because there are 365 days per year).
         //Disply Interest Earned
     }
