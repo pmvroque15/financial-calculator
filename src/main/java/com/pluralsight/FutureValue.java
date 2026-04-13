40ac31d7673436211a2bb50c9fe764c606cf868e
pmvroque15 <pmvroque@gmail.com>
2026-04-12 23:14:11 -0400

    Added instructions

--- src/main/java/com/pluralsight/FutureValue.java
+++ src/main/java/com/pluralsight/FutureValue.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
 public class FutureValue {
+    static void main(String[] args) {
+        // Instantiate scanner
+        // Declare Variables
+        //Make a greeting prompt
+        //Asking for Principal (P): This is the initial deposit amount
+        //Asking for Annual Interest Rate (r): The nominal annual interest rate in decimal form.
+        //Asking for Number of Years (t): The total number of years the deposit will earn interest.
+        //Asking for Days Per Year: Daily compounding assumes 365 days per year
+        // It would accept the deposit, interest rate, and number of years from the user
+        // It would display the future value and the total interest earned
+        //Display Total Number of Days:
+            // - This is 365 × t (because there are 365 days per year).
+        //Disply Interest Earned
+    }
+    //Method that would calculate the Future Value
+        // FV = P × (1 + (r / 365))^(365 × t)
 }
