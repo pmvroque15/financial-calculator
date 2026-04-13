25d8b14e9b1811053d13ec5e6cc46a9ea461622a
pmvroque15 <pmvroque@gmail.com>
2026-04-12 20:06:54 -0400

    cleaned mortgagecalculator code

--- src/main/java/com/pluralsight/MortgageCalculator.java
+++ src/main/java/com/pluralsight/MortgageCalculator.java
@@ -6,48 +6,45 @@ public class MortgageCalculator {
     static void main(String[] args) {
         //Instantiate Scanner
         Scanner scanner = new Scanner(System.in);
-        //Declare and Assign Variables
-        double total = 0;
-        double totalInterestPaid  = 0;
         //Prompt:Greeting
         System.out.println("Hello! Welcome to the Mortgage Calculator.");
         //Make a prompt that asks the first and last name of the client
         System.out.println("What is your first and last name? ");
         String userName = scanner.nextLine();
-        //Make a prompt that asks the total amount of the loan/Principal (P)
-        System.out.println("What is the total amount of the loan? ");
-        double userPrincipalAmount = scanner.nextDouble();
+        //Make a prompt that asks the monthlyPayment amount of the loan/Principal (P)
+        System.out.println("What is the total loan amount? ");
+        double userPrincipalAmount = Double.parseDouble(scanner.nextLine());
         //Make a prompt that asks how much is the annual interest rate (r)
         System.out.println("How much is the annual interest rate? ");
         double userAnnualInterestRate = scanner.nextDouble();
         userAnnualInterestRate = percentToDecimal(userAnnualInterestRate);
         //Ask the user how much loan terms in years (y)
         System.out.println("How much loan terms in years? ");
-        double userLoanTermYears = scanner.nextDouble();
+        int userLoanTermYears = scanner.nextInt();
         scanner.nextLine();
         //Monthly payment will be passing through a method and display it here
-        double userMonthlyPayments = numberOfMonthlyPayments(userLoanTermYears);
+        int userMonthlyPayments = numberOfMonthlyPayments(userLoanTermYears);
         //A method that will be called to calculate monthly interest Rate (i)
         double userMonthlyInterestRate = monthlyInterestRate(userAnnualInterestRate);
         System.out.println(" "); // Separation between prompts and the breakdown
         //Full Calculation
-        total += monthlyPaymentCalculator(
+        double monthlyPayment = monthlyPaymentCalculator(
                 userPrincipalAmount, userMonthlyPayments, userMonthlyInterestRate);
-        totalInterestPaid += totalInterest(total, userMonthlyPayments, userPrincipalAmount);
+        double totalInterestPaid = totalInterest(monthlyPayment, userMonthlyPayments, userPrincipalAmount);
 
         //Display Breakdown:
         System.out.printf("Hello, %s!\n", userName);
         System.out.print("-------------------------------------------\n");
+        System.out.printf("Principal Amount: $%.2f\n", userPrincipalAmount);
         System.out.println(
-                "Principal Amount: $" + (int)userPrincipalAmount + "\n"
-                        + "Annual Interest Rate: " + (userAnnualInterestRate * 100) + "%\n"
+                        "Annual Interest Rate: " + (userAnnualInterestRate * 100) + "%\n"
                         + "Loan Term: " + (int)userLoanTermYears + " years\n"
-                        + "Number of Monthly payments: " + (int)userMonthlyPayments + " months\n"
-                        + "Monthly Interest Rate: " + userMonthlyInterestRate
+                        + "Number of Monthly payments: " + (int)userMonthlyPayments + " months"
         );
-        //Displays Monthly payments and total interest paid
+        System.out.printf("Monthly Interest Rate: %.5f\n", userMonthlyInterestRate);
+        //Displays Monthly payments and monthlyPayment interest paid
         System.out.printf("Your Monthly payment for %s years will be: $%.2f/month\n",
-                            (int)userLoanTermYears, total);
+                            (int)userLoanTermYears, monthlyPayment);
         System.out.printf("Total Interest Paid: $%.2f", totalInterestPaid);
     }
 
@@ -58,7 +55,7 @@ public class MortgageCalculator {
     }
     //create a method that converts monthly payments (n)
         //12 * y
-    static double numberOfMonthlyPayments(double loanTermInYears) {
+    static int numberOfMonthlyPayments(int loanTermInYears) {
         return 12 * loanTermInYears;
     }
     //Monthly interest rate (i)
@@ -68,11 +65,12 @@ public class MortgageCalculator {
     //Calculates the monthly payment
         // M = P × (i * (1 + i)^n / ( (1 + i)^n ) - 1)
     static double monthlyPaymentCalculator(double principalAmount, double numberOfMonthlyPayments, double monthlyInterestRate ) {
-        return principalAmount * ((monthlyInterestRate * (Math.pow((1 + monthlyInterestRate), numberOfMonthlyPayments)))/ ((Math.pow((1 + monthlyInterestRate), numberOfMonthlyPayments)) - 1));
+        double factor = Math.pow((1 + monthlyInterestRate), numberOfMonthlyPayments);
+        return principalAmount * (monthlyInterestRate * (factor / (factor - 1)));
     }
     //Create a method that calculates the annual interest rate
         //Total interest = (M * n) - P
-    static double totalInterest(double totalMonthlyPayments, double numberOfMonthlyPayments, double principalAmount) {
-        return (totalMonthlyPayments * numberOfMonthlyPayments) - principalAmount;
+    static double totalInterest(double monthlyPayments, double numberOfPayments, double principal) {
+        return (monthlyPayments * numberOfPayments) - principal;
     }
 }
