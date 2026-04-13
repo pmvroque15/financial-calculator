22e6cf0c69a99d4350b29ebba58cb42a4670251a
pmvroque15 <pmvroque@gmail.com>
2026-04-13 12:36:39 -0400

    parsing all variables in the main

--- src/main/java/com/pluralsight/FutureValue.java
+++ src/main/java/com/pluralsight/FutureValue.java
@@ -16,40 +16,41 @@ public class FutureValue {
         String userName = scanner.nextLine();
         //Asking for Principal (P): This is the initial deposit amount
         System.out.println("What is the initial deposit amount? ");
-        double userPrincipalAmount = scanner.nextDouble();
+        double userPrincipalAmount = Double.parseDouble(scanner.nextLine());
         //Asking for Annual Interest Rate (r): The nominal annual interest rate in decimal form.
-        System.out.println("Annual Interest Rate: ");
-        double userAnnualInterestRate = scanner.nextDouble();
+        System.out.println("Enter annual interest rate (e.g. 5 for 5%): ");
+        double userAnnualInterestRate = Double.parseDouble(scanner.nextLine());
         userAnnualInterestRate = percentToDecimal(userAnnualInterestRate);
         //Asking for Number of Years (t): The total number of years the deposit will earn interest.
         System.out.println("Enter how many years you plan to leave the money in the account: ");
-        double userNumberOfYears = scanner.nextDouble();
+        int userNumberOfYears = Integer.parseInt(scanner.nextLine());
         System.out.println(" ");
-        //Days Per Year: Daily compounding assumes 365 days per year
-            //Display Total Number of Days:
-                // - This is 365 × t (because there are 365 days per year).
+
         System.out.printf("Hey %s!\n", userName);
         System.out.println("-------------------------");
         System.out.printf("Principal Amount: $%.2f\n", userPrincipalAmount);
-        System.out.printf("Annual Interest Rate %.4f\n", userAnnualInterestRate);
-        System.out.printf("Total number of years: %s years\n",  (int)userNumberOfYears);
+        System.out.printf("Annual Interest Rate %.2f%%\n", userAnnualInterestRate * 100);
+        System.out.printf("Total number of years: %s years\n",  userNumberOfYears);
         System.out.println("-------------------------");
-        double userYearToDays = yearsToDays(userNumberOfYears);
+        int userYearToDays = yearsToDays(userNumberOfYears);
         double futureValueTotal = futureValue(userPrincipalAmount, userAnnualInterestRate, userNumberOfYears);
         double userTotalInterestEarned = totalInterestEarned(futureValueTotal, userPrincipalAmount);
         // It would display the future value and the total interest earned
         System.out.printf("Future Value: $%.2f\n", futureValueTotal);
         System.out.printf("Total Interest Earned: $%.2f\n", userTotalInterestEarned);
+        //Days Per Year: Daily compounding assumes 365 days per year
+            //Display Total Number of Days:
+            // - This is 365 × t (because there are 365 days per year).
         System.out.printf("Total Number of Days: %s days\n", (int)userYearToDays);
 
     }
     //Method that would calculate the Future Value
         // FV = P × (1 + (r / 365))^(365 × t)
     static double futureValue(double principal, double annualInterestRate, double numberOfYears) {
-        double yearToDays = 365;
-        double exponent = (yearToDays * numberOfYears);
+        double daysPerYear = 365;
+        double exponent = (daysPerYear * numberOfYears);
         return (principal *
-                Math.pow((1 + (annualInterestRate / yearToDays)), exponent));
+                Math.pow((1 + (annualInterestRate / daysPerYear)), exponent));
     }
 
     static double percentToDecimal(double percent) {
@@ -57,7 +58,7 @@ public class FutureValue {
     }
     //Method that converts years to days
         //365 × t
-    static double yearsToDays(double numberOfYears) {
+    static int yearsToDays(int numberOfYears) {
         return 365 * numberOfYears; // 1 year = 365 days
     }
     // Total Interest Earned = FV - P
