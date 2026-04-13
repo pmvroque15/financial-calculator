ef003c7c95fc46d9c4312dbe14f501fdb57b290a
pmvroque15 <pmvroque@gmail.com>
2026-04-13 09:23:08 -0400

    Made greeting prompt

--- src/main/java/com/pluralsight/FutureValue.java
+++ src/main/java/com/pluralsight/FutureValue.java
@@ -1,11 +1,17 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FutureValue {
     static void main(String[] args) {
         // Instantiate scanner
+        Scanner scanner = new Scanner(System.in);
         // Declare Variables
         //Make a greeting prompt
+        System.out.println("Hello! Welcome to the Future Value calculator");
+        System.out.println("----------------------------------------------");
         //Asking for Principal (P): This is the initial deposit amount
+
         //Asking for Annual Interest Rate (r): The nominal annual interest rate in decimal form.
         //Asking for Number of Years (t): The total number of years the deposit will earn interest.
         //Asking for Days Per Year: Daily compounding assumes 365 days per year
