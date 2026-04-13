6cde22240be5754c152ac29585f28545263b795c
pmvroque15 <pmvroque@gmail.com>
2026-04-13 12:39:39 -0400

    cleaned code.

--- src/main/java/com/pluralsight/FutureValue.java
+++ src/main/java/com/pluralsight/FutureValue.java
@@ -11,36 +11,43 @@ public class FutureValue {
         //Make a greeting prompt
         System.out.println("Hello! Welcome to the Future Value calculator");
         System.out.println("----------------------------------------------");
+
         //prompt asks the user's name
         System.out.println("Type your first and last name: ");
         String userName = scanner.nextLine();
+
         //Asking for Principal (P): This is the initial deposit amount
         System.out.println("What is the initial deposit amount? ");
         double userPrincipalAmount = Double.parseDouble(scanner.nextLine());
+
         //Asking for Annual Interest Rate (r): The nominal annual interest rate in decimal form.
         System.out.println("Enter annual interest rate (e.g. 5 for 5%): ");
         double userAnnualInterestRate = Double.parseDouble(scanner.nextLine());
         userAnnualInterestRate = percentToDecimal(userAnnualInterestRate);
+
         //Asking for Number of Years (t): The total number of years the deposit will earn interest.
         System.out.println("Enter how many years you plan to leave the money in the account: ");
         int userNumberOfYears = Integer.parseInt(scanner.nextLine());
         System.out.println(" ");
 
+        //Display output; breakdown
         System.out.printf("Hey %s!\n", userName);
         System.out.println("-------------------------");
         System.out.printf("Principal Amount: $%.2f\n", userPrincipalAmount);
         System.out.printf("Annual Interest Rate %.2f%%\n", userAnnualInterestRate * 100);
         System.out.printf("Total number of years: %s years\n",  userNumberOfYears);
         System.out.println("-------------------------");
+
+        //Code calculation
         int userYearToDays = yearsToDays(userNumberOfYears);
         double futureValueTotal = futureValue(userPrincipalAmount, userAnnualInterestRate, userNumberOfYears);
         double userTotalInterestEarned = totalInterestEarned(futureValueTotal, userPrincipalAmount);
-        // It would display the future value and the total interest earned
+
+        //display the future value and the total interest earned
         System.out.printf("Future Value: $%.2f\n", futureValueTotal);
         System.out.printf("Total Interest Earned: $%.2f\n", userTotalInterestEarned);
+
         //Days Per Year: Daily compounding assumes 365 days per year
-            //Display Total Number of Days:
-            // - This is 365 × t (because there are 365 days per year).
         System.out.printf("Total Number of Days: %s days\n", (int)userYearToDays);
 
     }
